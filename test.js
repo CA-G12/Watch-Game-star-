088b334bbc69900175b7e51b663d6f4442177b01
mohmmed23 <mhmmedben3@gmail.com>
2022-08-02 15:34:31 +0300

    add describe function relted #3

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 const { deleteItem } = require('./logic')
 
-test('Refactor our delete item function so it is pure.', function () {
-  expect(deleteItem([{ id: 1 }, { id: 2 }], { id: 1 })).toEqual([{ id: 2 }])
-})
-test('Refactor our delete item function so it is pure.', function () {
-  expect(deleteItem([{ id: 1 }, { id: 2 }], { id: 3 })).toEqual([
-    { id: 1 },
-    { id: 2 },
-  ])
+describe('delete function pure', () => {
+  test('Refactor our delete item function so it is pure.', function () {
+    expect(deleteItem([{ id: 1 }, { id: 2 }], { id: 1 })).toEqual([{ id: 2 }])
+  })
+  test('Refactor our delete item function so it is pure.', function () {
+    expect(deleteItem([{ id: 1 }, { id: 2 }], { id: 3 })).toEqual([
+      { id: 1 },
+      { id: 2 },
+    ])
+  })
 })
