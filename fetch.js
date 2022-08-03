502b798013e3b40b18ff941026c3aff95fc8690f
mohmmed23 <mhmmedben3@gmail.com>
2022-08-03 11:51:55 +0300

    create anime page [html,css ,js] ,create public dom file releted #12

--- fetch.js
+++ fetch.js
@@ -1,23 +1,4 @@
-fetchingGame = (url, cb) => {
-  let xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      let data = JSON.parse(xhr.responseText)
-      console.log(data)
-      // cb(data)
-    }
-  }
-  xhr.open('GET', url)
-  xhr.setRequestHeader(
-    'x-rapidapi-host',
-    'free-to-play-games-database.p.rapidapi.com',
-  )
-  xhr.setRequestHeader(
-    'x-rapidapi-key',
-    '93b272acdamsh10e28390fcb3114p1a8d21jsn6bd1fef7e35a',
-  )
-  xhr.send()
-}
+
 fetchingMovie = (url, cb) => {
   let xhr = new XMLHttpRequest()
   xhr.onreadystatechange = function () {
@@ -30,3 +11,4 @@ fetchingMovie = (url, cb) => {
 
   xhr.send()
 }
+
